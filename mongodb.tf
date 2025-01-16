2c30f54c8caf587578ff2e998bf225ba855f5c80
Louis <Louismmaduabuchi1@gmail.com>
2025-01-16 10:51:05 +0000

    infrastructure deployment

---
+++ mongodb.tf
@@ -0,0 +1,27 @@
+resource "google_sql_database_instance" "mysql_instance" {
+  name             = "fusion-mysql-db-instance"
+  database_version = "MYSQL_8_0"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "default"
+        value = "0.0.0.0/0" # Replace with specific IP ranges for security
+      }
+    }
+  }
+}
+
+resource "google_sql_database" "database" {
+  name     = "fusion_database"
+  instance = google_sql_database_instance.mysql_instance.name
+}
+
+resource "google_sql_user" "db_user" {
+  name     = "fusion-connect"
+  instance = google_sql_database_instance.mysql_instance.name
+  password = var.db-password-base64
+}
